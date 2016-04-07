34a62b58a3060a81b6b77bad2ec663d7ce9b4eca
Casper <205720@student.fontys.nl>
2016-04-07 10:37:43 +0200

    Commiting project to git.

--- SofwareCraftsmanship/src/main/java/com/fontys/softwarecraftsmanship/Part.java
+++ SofwareCraftsmanship/src/main/java/com/fontys/softwarecraftsmanship/Part.java
@@ -7,36 +7,7 @@ public class Part {
     private String subtype = null;
     private int size = -1;
     private double speed = -1.0;
-    
-    public Part(String title, double price, String subtype, int size, double speed) {
-        this.title = title;
-        this.price = price;
-        this.subtype = subtype;
-        this.size = size;
-        this.speed = speed;
-    }
-    
-    // Memory
-    public Part(String title, double price, String subtype, int size) {
-        this.title = title;
-        this.price = price;
-        this.subtype = subtype;
-        this.size = size;
-    }
-    
-    // Casing
-    public Part(String title, double price, String subtype) {
-        this.title = title;
-        this.price = price;
-        this.subtype = subtype;
-    }
-    
-    // Processor
-    public Part(String title, double price, double speed) {
-        this.title = title;
-        this.price = price;
-        this.speed = speed;
-    }
+   
     
     // Motherboard
     public Part(String title, double price) {
